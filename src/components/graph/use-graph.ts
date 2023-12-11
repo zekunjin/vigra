cdfc011897955b9cc291cddc28c6e2a2afd754f8
zekunjin <zekun.jin@foxmail.com>
2023-12-11 17:11:42 +0800

    chore: rm interactjs

--- src/components/graph/use-graph.ts
+++ src/components/graph/use-graph.ts
@@ -1,15 +1,11 @@
 import { computed, ref, useSlots, watch } from 'vue'
 import type { Renderer } from '@antv/g-canvas'
-import { Group, Rect } from '@antv/g'
-import interact from 'interactjs'
 import { diff } from 'ohash'
 import { pickChildren } from '../../utils/vue'
 import { Node, type NodeProps } from '../node'
-import { defineNode } from '../node/use-node'
+import { bindDraggableEvent, defineNode } from '../node/use-node'
 import type { GraphProps } from './graph'
 import { useCanvas } from './use-canvas'
-
-const nodes: Record<string, any> = {}
 
 export interface UseGraphProps extends GraphProps { }
 
@@ -37,20 +33,11 @@ export const useGraph = (props: UseGraphProps) => {
   }, { immediate: true })
 
   const insert = (node: NodeProps) => {
-    if (!canvas.value || !node.id) { return }
+    if (!canvas.value) { return }
     const _n = defineNode(node)
-    nodes[node.id] = _n
-    canvas.value.appendChild(nodes[node.id])
-
-    if (node.draggable) {
-      interact(nodes[node.id] as unknown as HTMLElement, { context: canvas.value?.document as any }).draggable({
-        onmove (event) {
-          const { dx, dy } = event
-          if (!node.id) { return }
-          nodes[node.id].translateLocal(dx, dy)
-        }
-      })
-    }
+    if (!_n) { return }
+    canvas.value.appendChild(_n)
+    if (node.draggable) { bindDraggableEvent(canvas.value)(_n) }
   }
 
   return { domRef, canvas }
--- src/components/node/use-node.ts
+++ src/components/node/use-node.ts
@@ -1,6 +1,40 @@
-import { Rect } from '@antv/g'
+import { type Canvas, Rect, type DisplayObject } from '@antv/g'
 import type { NodeProps } from './node'
 
 export const defineNode = (props: NodeProps) => {
-  return new Rect({ id: props.id, style: props.style as any })
+  if (!props.id) { return }
+  return new Rect({
+    id: props.id,
+    style: {
+      cursor: props.draggable ? 'move' : 'auto',
+      ...props.style
+    } as any
+  })
+}
+
+export const bindDraggableEvent = (canvas?: Canvas) => (node: DisplayObject) => {
+  if (!canvas) { return }
+  node.addEventListener('pointerdown', (event: any) => {
+    const shiftX = event.clientX - node.getBoundingClientRect().left
+    const shiftY = event.clientY - node.getBoundingClientRect().top
+
+    const moveAt = (canvasX: number, canvasY: number) => {
+      node.style.x = canvasX - shiftX + 'px'
+      node.style.y = canvasY - shiftY + 'px'
+    }
+
+    moveAt(event.canvasX, event.canvasY)
+
+    const onMouseMove = (event: any) => {
+      moveAt(event.canvasX, event.canvasY)
+    }
+
+    canvas.document.addEventListener('pointermove', onMouseMove)
+
+    node.addEventListener(
+      'pointerup',
+      () => { canvas.document.removeEventListener('pointermove', onMouseMove) },
+      { once: true }
+    )
+  })
 }
